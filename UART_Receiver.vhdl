3e9fccf5e2d4405625f885473adf76f4b61e3670
ElHuaco <alexhuac@gmail.com>
2022-01-12 13:17:23 +0100

    Impossible Synth

--- UART_Receiver.vhdl
+++ UART_Receiver.vhdl
@@ -1,12 +1,11 @@
 library IEEE;
-use IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity UART_Receiver is
     port(clk : in std_logic;
         data : in std_logic;
-        valid : out std_logic;
+        valid : out std_logic := '0';
         y : out std_logic_vector(7 downto 0));
 end entity UART_Receiver;
 
@@ -15,19 +14,19 @@ architecture behave of UART_Receiver is
         port(clk : in std_logic;
             data : in std_logic;
             baud : in std_logic;
-            valid : out std_logic <= '0';
+            valid : out std_logic;
             y : out std_logic_vector(7 downto 0));
     end component;
 
     component sampler_UART_RX is
         port(clk : in std_logic;
             data : in std_logic;
-            baud : out std_logic);
+            baudrate : out std_logic);
     end component;
 
     signal baud : std_logic;
 
 begin 
     state_machine : rec port map(clk=>clk, data=>data, valid=>valid, baud=>baud, y=>y);
-    sample : sample_gen port map(clk=>clk, data=>data, baud=>baud);
+    sample : sampler_UART_RX port map(clk=>clk, data=>data, baudrate=>baud);
 end behave;
--- sampler_UART_RX.vhdl
+++ sampler_UART_RX.vhdl
@@ -1,34 +1,32 @@
 library IEEE;
-use library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity sampler is
+entity sampler_UART_RX is
 
 	generic(constant lambda : integer := 868);
 
 	port(clk		: in std_logic;
-		uart_rx		: in std_logic;
+		data		: in std_logic;
 		baudrate	: out std_logic);
 
-end sampler;
+end sampler_UART_RX;
 
-architecture my_sampler of sampler is
+architecture my_sampler of sampler_UART_RX is
 
-	signal bau_cnt	: integer;
-	signal dly_cnt : integer <= lambda;
+	signal bau_cnt	: integer := 0;
+	signal dly_cnt : integer := lambda;
 	signal pls_cnt	: integer;
-	signal enable	: std_logic <= '0';
-	signal pulse	: std_logic <= '0';
+	signal enable	: std_logic := '0';
+	signal pulse	: std_logic := '0';
 
 begin
 	start_detector	: process(clk)
 	begin
 		if rising_edge(clk) then
 			if enable = '0' then
-				if uart_rx = '0' then
+				if data = '0' then
 					enable <= '1';
-					bau_cnt <= 0;
 					pls_cnt <= 0;
 					pulse <= '1';
 				end if;
@@ -43,6 +41,7 @@ begin
 				if pls_cnt = 9 then
 					enable <= '0';
 					pulse <= '0';
+					bau_cnt <= 0;
 				elsif bau_cnt < lambda then
 					bau_cnt <= bau_cnt + 1;
 					pulse <= '0';
@@ -55,10 +54,10 @@ begin
 		end if;
 	end process;
 
-	delayer			: process(clk)
+	delayer			: process(clk, pulse)
 	begin
 		if pulse = '1' then
-			dly_cnt = 0;
+			dly_cnt <= 0;
 		elsif rising_edge(clk) then
 			if dly_cnt < 434 then
 				dly_cnt <= dly_cnt + 1;
